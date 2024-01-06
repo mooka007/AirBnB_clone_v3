a0a04ee72a15e0d54c88b49954ce6eb89ea827e4
mooka007 <mooka3084@gmail.com>
2024-01-06 15:18:04 -0800

    task 4 some stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,37 +1,46 @@
 #!/usr/bin/python3
-"""define routes of blueprint
+"""
+index
 """
 
+from flask import jsonify
 from api.v1.views import app_views
+
 from models import storage
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.place import Place
-from models.review import Review
-from models.user import User
 
 
-@app_views.route("/status", strict_slashes=False, methods=["GET"])
+@app_views.route("/status", methods=['GET'], strict_slashes=False)
 def status():
-    return {
-        "status": "OK",
+    """
+    status route
+    :return: response with json
+    """
+    data = {
+        "status": "OK"
     }
 
+    resp = jsonify(data)
+    resp.status_code = 200
 
-@app_views.route("/stats", strict_slashes=False, methods=["GET"])
+    return resp
+
+
+@app_views.route("/stats", methods=['GET'], strict_slashes=False)
 def stats():
-    amenities = storage.count(Amenity)
-    cities = storage.count(City)
-    places = storage.count(Place)
-    reviews = storage.count(Review)
-    states = storage.count(State)
-    users = storage.count(User)
-    return {
-        "amenities": amenities,
-        "cities": cities,
-        "places": places,
-        "reviews": reviews,
-        "states": states,
-        "users": users,
+    """
+    stats of all objs route
+    :return: json of all objs
+    """
+    data = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User"),
     }
+
+    resp = jsonify(data)
+    resp.status_code = 200
+
+    return resp
